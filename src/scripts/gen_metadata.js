bf061958ef83156e12228b67adf8410014b14429
Jake Weathington <kennethjweathington@gmail.com>
2020-04-19 21:46:15 -0400

    Added db disconnect at end of gen_metadata

--- src/scripts/gen_metadata.js
+++ src/scripts/gen_metadata.js
@@ -12,41 +12,55 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 //#region metadata creation
 
+const promises = [];
+
 const CounterModel = require('../models/counter');
 const counterArr = ['deaths', 'boops']
 for (let i = 0; i < counterArr.length; i++) {
-  createModelIfNotExist(counterArr[i], CounterModel, {name:counterArr[i]}, {name: counterArr[i], count: 0, scoreboard: []});
+  const modelPromise = createModelIfNotExist(counterArr[i], CounterModel,
+    { name: counterArr[i] }, { name: counterArr[i], count: 0, scoreboard: [] });
+  promises.push(modelPromise);
 }
+
+Promise.all(promises).then(() => {
+  console.log("Loading done.")
+  mongoose.disconnect().then(() => console.log("Disconnected from DB."));
+});
+
 
 //#endregion metadata creation
 
 //#region helper functions
 
 function createModelIfNotExist(name, model, srchObj, createObj) {
-    model.findOne(srchObj, function(err, result) {
-        callback(err, result, name, createObj);
+  return new Promise((resolve, reject) => {
+    model.findOne(srchObj, (err, result) => {
+      createModelCallback(err, result, name, createObj);
+      if (err) reject(err);
+      else resolve();
     });
+  })
 }
 
-function callback(err, result, name, createObj) {
-    const err_msg = `Error loading ${name}.`;
-    const succ_msg = `Successfully created ${name}!`;
-  
-    if(err) handleError(err_msg);
-    else if(!result) {
-        CounterModel.create(createObj, function(err, result) {
-            if(err || !result) handleError(err_msg);
-            else {
-              console.log(succ_msg);
-            }
-        })
-    } else console.log(`${name} already exists.`)
-  }
+function createModelCallback(err, result, name, createObj) {
+  const err_msg = `Error loading ${name}.`;
+  const succ_msg = `Successfully created ${name}!`;
 
-  function handleError(msg) {
-    console.log(msg);
-    return;
-  }
+  if (err) handleError(err_msg);
+  else if (!result) {
+    CounterModel.create(createObj, function (err, result) {
+      if (err || !result) handleError(err_msg);
+      else {
+        console.log(succ_msg);
+      }
+    })
+  } else console.log(`${name} already exists.`)
+}
+
+function handleError(msg) {
+  console.log(msg);
+  return;
+}
 
 
 //#endregion helper functions
